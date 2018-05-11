374aa07c2da3b20d686d4ed633fe99ada349c6b6
Andre Holzner <holzner@mbp-ah.dyndns.cern.ch>
2018-05-11 17:10:28 +0200

    added echo banners for the different steps

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,11 +1,18 @@
 driver_version=390.48
 cuda="cuda_9.0.176_384.81_linux-run"
 cudnn="cudnn-9.0-linux-x64-v7.1.tar"
+
+echo ----------
+echo installing NVIDIA driver
+echo ----------
 sh /mnt/NVIDIA-Linux-x86_64-$driver_version.run -x
 mv NVIDIA-Linux-x86_64-$driver_version /usr/local/
 sh /mnt/links.sh $driver_version
 
-sh /mnt/$cuda  --toolkit --noprompt
+echo ----------
+echo installing CUDA
+echo ----------
+
 tar xvf /mnt/$cudnn -C /usr/local
 
 driver_path=/usr/local/NVIDIA-Linux-x86_64-$driver_version
