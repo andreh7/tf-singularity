e90b8c1493d5e24b1cf8cbb9c100860ab0feeaa4
Andre Holzner <holzner@mbp-ah.dyndns.cern.ch>
2018-05-11 17:12:03 +0200

    increased size of singularity image to be created

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 IMG="tf-gpu.img"
 DEF="tf-gpu.def"
 sudo rm -f $IMG
-sudo singularity create -s 4000 $IMG
+sudo singularity create -s 6000 $IMG
 sudo singularity bootstrap $IMG $DEF
 sudo singularity exec -B `pwd`:/mnt -w $IMG sh /mnt/tensorflow.sh 
 sudo singularity exec  -B `pwd`:/mnt -w $IMG sh /mnt/sherlock.sh
